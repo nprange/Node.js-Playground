05c321f562f3d7488ef6012133385a405dba7acb
Nicolas Prange <52366220+nprange@users.noreply.github.com>
2025-01-18 02:09:36 +0000

    feat: add `regex` to identify `Route Params` and `delete user`

--- Fundamentals Node.js/src/server.js
+++ Fundamentals Node.js/src/server.js
@@ -1,6 +1,6 @@
 import http from "http";
 import { json } from "./middlewares/json.js";
-import { routes } from "./route.js";
+import { routes } from "./routes.js";
 
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
@@ -8,10 +8,12 @@ const server = http.createServer(async (request, response) => {
   await json(request, response);
 
   const route = routes.find((route) => {
-    return route.method === method && route.path === url;
+    return route.method === method && route.path.test(url);
   });
 
   if (route) {
+    const routeParams = request.url.match(route.path);
+    request.params = { ...routeParams.groups };
     return route.handler(request, response);
   }
 
